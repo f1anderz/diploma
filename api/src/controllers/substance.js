933c045ebce53fd02fc213c8688a3c1a8b989fdf
StasStepaniuk <stas.stepaniuk@precoro.com>
2025-05-28 00:45:01 +0300

    fix sleect

--- api/src/controllers/substance.js
+++ api/src/controllers/substance.js
@@ -35,7 +35,7 @@ exports.getSubstancePreparates = async (req, res) => {
 
 exports.getSubstanceSynonyms = async (req, res) => {
   await fdb.query(
-    `SELECT * FROM v_substance_synonyms WHERE SUBST_ID = ${req.params.id}`,
+    `SELECT ID, U_NAME, E_NAME FROM v_substance_synonyms WHERE SUBST_ID = ${req.params.id}`,
     (err, result) => {
       if (err) {
         return res.status(500).send({ msg: "Internal error", error: err });
