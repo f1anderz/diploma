c1ba980bf291b41ec38fb95aa9c13638965ab899
StasStepaniuk <stas.stepaniuk@precoro.com>
2025-05-27 23:48:43 +0300

    add cast

--- api/src/controllers/others.js
+++ api/src/controllers/others.js
@@ -2,7 +2,7 @@ const fdb = require('../../config/dbConnection');
 
 exports.getRequirementReglament = async (req,res) => {
   await fdb.query(
-    `SELECT * FROM v_requirement_reglament`,
+    `SELECT ID, CAST(NORM_DOC AS VARCHAR(500) CHARACTER SET WIN1251) AS NORM_DOC, CAST(DOC_REG AS VARCHAR(500) CHARACTER SET WIN1251) AS DOC_REG FROM v_requirement_reglament`,
     (err, result) => {
       if (err) {
         return res.status(500).send({ msg: "Internal error", error: err });
@@ -14,7 +14,7 @@ exports.getRequirementReglament = async (req,res) => {
 
 exports.getOperatorSkills = async (req,res) => {
   await fdb.query(
-    `SELECT * FROM v_operator_skills`,
+    `SELECT ID, CAST(MEASUREMENT AS VARCHAR(500) CHARACTER SET WIN1251) AS MEASUREMENT, CAST(PREPARATION AS VARCHAR(500) CHARACTER SET WIN1251) AS PREPARATION FROM v_operator_skills`,
     (err, result) => {
       if (err) {
         return res.status(500).send({ msg: "Internal error", error: err });
